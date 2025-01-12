6b7a761781f532c8ff8227432ec4d39783cf669b
Vlad Razdobudko <iam@razdobudko.ru>
2025-01-12 19:34:57 +0300

    [26] [Time Beats: 100.00%] [Memory Beats: 51.22%] - LeetPush

---
+++ 26-Remove-Duplicates-from-Sorted-Array.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    uniqueNumIndex := 1
+    currNum := nums[0]
+
+    for _, v := range nums {
+        if v == currNum {
+            continue
+        }
+
+        nums[uniqueNumIndex] = v
+        uniqueNumIndex++
+        currNum = v
+    }
+
+    return uniqueNumIndex
+}
