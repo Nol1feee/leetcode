e0ae59b3bbb1647913a04297dbae0dd0271778d8
Vlad Razdobudko <iam@razdobudko.ru>
2025-01-18 13:40:42 +0300

    Time: 0 ms (100.00%) | Memory: 5.4 MB (73.56%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.go
@@ -0,0 +1,18 @@
+func shuffle(nums []int, n int) []int {
+    res := make([]int, len(nums))
+    a1 := nums[:n]
+    a2 := nums[n:]
+    index := 0
+
+    for i := 0; i < len(nums); i++ {
+        if i % 2 == 0 {
+            res[i] = a1[index]
+            continue
+        }
+        res[i] = a2[index]
+        index++ 
+    }
+
+    return res
+     
+}
