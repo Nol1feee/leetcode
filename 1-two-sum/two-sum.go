6d3314f27f499d295038791a2577f21b99fefe23
Vlad Razdobudko <iam@razdobudko.ru>
2025-04-15 23:00:34 +0300

    Time: 0 ms (100.00%) | Memory: 5.9 MB (61.71%) - LeetSync

---
+++ 1-two-sum/two-sum.go
@@ -0,0 +1,21 @@
+// что требуется:
+//   - вернуть индекс значений из массива, сумма значений которых равна 'target' 
+// как решить:
+//   - самое простое - перебор, но сложность в таком случае nˆ2
+//   - target - currNum -> targetForUs; for nums {map[val]index} return
+//     - проблемы с одинаковыми значениями на разных индексах
+//       - можно вообще за 1 итерацию с мапой
+// итог:
+//   - изи, можно на собесе стажеру/джуну дать
+
+func twoSum(nums []int, target int) []int {
+    dict := make(map[int]int, len(nums))
+    for i, v := range nums {
+        if index, ok := dict[target - v]; ok {
+            return []int{index, i}
+        }
+        dict[v] = i
+    }
+
+    return []int{}
+}
